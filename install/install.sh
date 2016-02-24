e2b9135fc7f4d94b16fdc4880f4b598c648cb55e
Gauthier Voron <gauthier.voron@lip6.fr>
2016-02-24 16:44:44 +0100

    add INSTALL

---
+++ install/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Install the necessary files for the benchmark platform to be usable
+# Install the control-payload
+# Install the null-payload
+# Install the npb-payload
+# Install the parsec-payload
+# Install the vm-hvm-base
+
+IPATH="`realpath \"${0%/*}\"`"
+PATH="$IPATH":"$PATH"
+
+install-control.sh
+install-null.sh
+install-npb.sh
+install-parsec.sh
+install-vm-hvm-base.sh
